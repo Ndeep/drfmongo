c1c70a68176de8d8933ffe87024aa4cd3c24d26e
Deep <kandpal5@gmail.com>
2018-09-11 11:38:14 +0530

    Update models.py

--- hitechdemo/rest_api_demo/models.py
+++ hitechdemo/rest_api_demo/models.py
@@ -48,3 +48,21 @@ class Tool(Document):
     successCodes = fields.ListField(fields.IntField(), required=False)
     temporaryFailCodes = fields.ListField(fields.IntField(), required=False)
     permanentFailCodes = fields.ListField(fields.IntField(), required=False)
+    
+class AccesslogsModel(models.Model):
+    sys_id = models.AutoField(primary_key=True, null=False, blank=True)
+    session_key = models.CharField(max_length=1024, null=False, blank=True)
+    path = models.CharField(max_length=1024, null=False, blank=True)
+    method = models.CharField(max_length=8, null=False, blank=True)
+    data = models.TextField(null=True, blank=True)
+    ip_address = models.CharField(max_length=45, null=False, blank=True)
+    referrer = models.CharField(max_length=512, null=True, blank=True)
+    timestamp = models.DateTimeField(null=False, blank=True)
+
+    def __str__(self):
+        return self.method
+
+    class Meta:
+        app_label = "django_server_access_logs"
+        db_table = "access_logs"
+    
